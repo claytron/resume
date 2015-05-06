d7c43aa727b86a3a38b2f2ba316eb603fb2e5f6f
Clayton Parker <clayton@sixfeetup.com>
2015-05-06 14:58:15 -0400

    script to build pdf using wkhtmltopdf

---
+++ build_pdf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# TODO: move this stuff into the make file
+
+[ -n "$1" ] && height="$1" || height='840mm'
+
+make clean && make html
+
+rsync -av _build/html/ claytron.com:/usr/local/www/data/resume
+
+wkhtmltopdf --user-style-sheet pdf.css  -B 0 -L 0 -R 0 -T 0 --page-width 210mm --page-height $height --disable-forms page http://claytron.com/static/resume/ _build/html/resume.pdf
+
+rsync -av _build/html/ claytron.com:/usr/local/www/data/resume
+
+echo
+echo 'WARNING: Check the bottom of the file to see if it made a page break.'
+echo 'Then pass in the height accordingly'
