3fb930fefdf7057bae821ce40545a3ac3ce8701d
Clayton Parker <clayton@sixfeetup.com>
2015-05-07 14:25:07 -0400

    add a favicon

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ html_title = 'Resume'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/favicon.ico'
 
 html_static_path = ['_static']
 
