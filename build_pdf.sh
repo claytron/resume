15f624280d28fa94e11041b988cea43cc1c89ec2
Clayton Parker <clayton@sixfeetup.com>
2015-05-07 12:04:21 -0400

    PDF gen tweaks

--- build_pdf.sh
+++ build_pdf.sh
@@ -2,13 +2,13 @@
 
 # TODO: move this stuff into the make file
 
-[ -n "$1" ] && height="$1" || height='840mm'
+[ -n "$1" ] && height="$1" || height='860mm'
 
 make clean && make html
 
 rsync -av _build/html/ claytron.com:/usr/local/www/data/resume
 
-wkhtmltopdf --user-style-sheet pdf.css  -B 0 -L 0 -R 0 -T 0 --page-width 210mm --page-height $height --disable-forms page http://claytron.com/static/resume/ _build/html/resume.pdf
+wkhtmltopdf --user-style-sheet pdf.css  -B 0 -L 0 -R 0 -T 0 --page-width 210mm --page-height $height --disable-forms page http://claytron.com/static/resume/ _build/html/clayton_parker_resume.pdf
 
 rsync -av _build/html/ claytron.com:/usr/local/www/data/resume
 
